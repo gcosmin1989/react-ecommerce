f5622a97b18d3636603c1065e439b115f919fbbe
gcosmin1989 <gherghina.cosmin@yahoo.com>
2022-11-14 22:09:13 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer() {
+	const currentYear = new Date();
 	return (
-		<div>
-			<Link to="/about">About</Link>
-			<Link to="/">Home</Link>
+		<div className="py-3 my-4">
+			<div className="nav justify-content-center border-bottom pb-3 mb-3 ">
+				<Link className=" nav-link px-2 text-muted" to="/about">
+					About
+				</Link>
+
+				<Link className="nav-link px-2 text-muted" to="/">
+					Home
+				</Link>
+			</div>
+			<p className="text-center text-muted">@ {currentYear.getFullYear()} Company, Inc</p>
 		</div>
 	);
 }
